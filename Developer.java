5d450f8ec8121e982ce66891c2a21abedcaa9562
Nathaniel Richardson <richa219@miamioh.edu>
2020-10-14 17:34:15 -0400

    Added relevent javadoc comments

--- Developer.java
+++ Developer.java
@@ -3,13 +3,21 @@ public class Developer extends User {
 	
 	private String company;
 
-
+	/**
+	 * Creates a developer object using account credentials.
+	 * @param user The username of the developer account.
+	 * @param pword The password for a target user account.
+	 */
 	public Developer(String user,
 			    String pword) {
 		super(user,pword);
 		
 	}
 	
+	/**
+	 * Updates an application that the developer has access to.
+	 * @param app The application data that should overwrite the previous data.
+	 */
 	public void updateApp(Application app) {
 		if (app.company.equals(company)) {
 			//code to allow developers to select an field to alter, alter it, or go back to the selection screen
