689159161ebe01f53e12209eb70b029e89ce0016
hermanbh <72168674+hermanbh@users.noreply.github.com>
2020-10-14 17:23:59 -0400

    Update AppOrganizationTool.java

--- AppOrganizationTool.java
+++ AppOrganizationTool.java
@@ -1,5 +1,10 @@
 public class AppOrganizationTool {
+    
+    public ArrayList<Application> apps = new ArrayList<>();
+    
+    public Queue<Application> requests = new Queue<>();
+    
     public static void main() {
-
+        
     }
 }
