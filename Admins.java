0dd7cb44ad27f2f7e776fe94221befeff756ce51
hermanbh <72168674+hermanbh@users.noreply.github.com>
2020-10-14 17:51:28 -0400

    Update Admins.java

--- Admins.java
+++ Admins.java
@@ -6,13 +6,14 @@ public class Admins extends Moderator {
 		
 	}
 	
-	public void viewRequest( ) {
-		
+	public void viewRequest(Application app) {
+		// shows the details of the app, for now this is similar
+		System.out.println(app.toArray().toString());
 		
 	}
 	
-	public void approveRequest() {
-		
+	public void approveRequest(AppOrganizationalTool tool, Application app) {
+		tool.apps.add(app);
 		
 	}
 
