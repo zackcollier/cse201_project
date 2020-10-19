6ac4a15679f0052128516d15753dac156f8954ab
richa219 <62719601+richa219@users.noreply.github.com>
2020-10-19 18:27:02 -0400

    Removed unneeded method

--- GraphicalInterface.java
+++ GraphicalInterface.java
@@ -11,10 +11,6 @@ public class GraphicalInterface {
     public void filter(String arg0) {
         // Display all apps with apps that do not have a specific 'thing' 
     }
-    
-    public void search(String term) {
-        // Display apps that contain a specific search term, there should probably be a weight to prioritize app fields that are exact matches.
-    }
   
     public void void commentSection() {
         // Display the comment section of the current app  
