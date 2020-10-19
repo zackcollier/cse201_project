5288b77e466bb27c54444d179297fd71b09dfa14
hermanbh <72168674+hermanbh@users.noreply.github.com>
2020-10-19 16:55:37 -0400

    Update AppOrganizationTool.java

--- AppOrganizationTool.java
+++ AppOrganizationTool.java
@@ -1,18 +1,34 @@
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class AppOrganizationTool {
+
+	ArrayList<Application> apps = new ArrayList<>();
+	Queue<Application> requests = new LinkedBlockingQueue<>();
+	
+	public AppOrganizationTool()  {}
     
-    public ArrayList<Application> apps = new ArrayList<>();
-        
-    public Queue<Application> requests = new Queue<>();
-    
-    public static void main() {
-        Application app1("App 1 Name", "App 1 Description", "App 1 Company", "App 1 Platforms", "App 1 Version", "App 1 Genre");
-        Application app2("App 2 Name", "App 2 Description", "App 2 Company", "App 2 Platforms", "App 2 Version", "App 2 Genre");
+	public ArrayList<Application> getApps() {
+		return apps;
+	}
+	
+	public Queue<Application> getRequests() {
+		return requests;
+	}
+	
+    public static void main(String args[]) {
+        Application app1 = new Application("App 1 Name", "App 1 Description", "App 1 Company", "App 1 Platforms", "App 1 Version", "App 1 Genre");
+        Application app2 = new Application("App 2 Name", "App 2 Description", "App 2 Company", "App 2 Platforms", "App 2 Version", "App 2 Genre");
         Admin a1 = new Admin("username", "password");
-        requests.add(app1);
-        requests.add(app2);
-        a1.viewRequest(requests.remove());
-        a1.viewRequest(requests.remove());
-        System.out.println(apps.get(0).toArray());
-        System.out.println(apps.get(1).toArray());
+        AppOrganizationTool AOT = new AppOrganizationTool();
+        AOT.requests.add(app1);
+        AOT.requests.add(app2);
+        a1.viewRequest(AOT.getRequests().peek());
+        a1.approveRequest(AOT, AOT.getRequests().remove());
+        a1.viewRequest(AOT.getRequests().peek());
+        a1.approveRequest(AOT, AOT.getRequests().remove());
+        AOT.getApps().get(0).printDetails();
+        AOT.getApps().get(1).printDetails();
     }
 }
