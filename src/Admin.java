4197c3d88fde2688242eec2e9646462da467a017
hermanbh <72168674+hermanbh@users.noreply.github.com>
2020-11-10 18:03:58 -0500

    Update Admin.java
    Added Documentation

--- src/Admin.java
+++ src/Admin.java
@@ -1,20 +1,29 @@
 
 public class Admin extends Moderator {
 
+	/**
+	 * Creates an admin object using account credentials.
+	 * @param user The username of the admin account.
+	 * @param pword The password for a target user account.
+	 */
 	public Admin(String user, String pword) {
 		super(user, pword);
-		
 	}
 	
+	/**
+	 * Shows the details of an app that was submitted for approval.
+	 * @param The application to be reviewed.
+	 */
 	public void viewRequest(Application app) {
-		// shows the details of the app, for now this is similar
 		app.printDetails();
-		
 	}
 	
+	/**
+	 * Approves an app into the app.
+	 * @param The Tool where the app will be sent to.
+	 * @param The application to be reviewed.
+	 */
 	public void approveRequest(AppOrganizationTool tool, Application app) {
-		tool.apps.add(app);
-		
+		tool.apps.add(app);	
 	}
-
 }
