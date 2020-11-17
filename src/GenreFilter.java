7c53e2374e007feb9017b497bdedbe7986c88bda
Qi Feng <49265120+Rivendell-FQ@users.noreply.github.com>
2020-11-17 16:34:11 -0500

    comments

--- src/GenreFilter.java
+++ src/GenreFilter.java
@@ -3,21 +3,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class GenreFilter {
-
 	private ArrayList<String> genres;
 	private ArrayList<ArrayList<String>> data;
-
+ 
 	public GenreFilter() {
 		
 		this.genres = new ArrayList<String>();
 		data = new ArrayList<ArrayList<String>>(AppOrganizationTool.sortGenreFilterData);
 		
 	}
-	
+	/*
+	* make the arraylist for the genres
+	*/
 	public void setGenres(ArrayList<String> genres) {
 		this.genres = new ArrayList<String>(genres);
 	}
-
+      /*
+      * return the arraylist that are filtered
+      * make the genres of the filter been filter
+      */
 	public ArrayList<ArrayList<String>> getFilteredGenres() {
 		ArrayList<ArrayList<String>> filtered = new ArrayList<ArrayList<String>>();
 		System.out.println("FilterGenre");
