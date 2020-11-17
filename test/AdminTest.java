ec0a9663daba6abcc7e4ca94168369e7b146335f
richa219 <62719601+richa219@users.noreply.github.com>
2020-11-17 16:01:45 -0500

    Create AdminTest.java

---
+++ test/AdminTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+// viewRequest method should be entirely visual
+public class AdminTest {
+	private AppOrganizationTool AOT;
+	private Application APP;
+	private Developer dev;
+	
+	@Before
+	public void setUp() {
+		AOT = new AppOrganizationTool();
+		APP = new Application("App", "", "sampleCompany", "", "", "");
+		dev = new Developer("test", "test123", "testCompany");
+	}
+	
+	@Test
+	public void testApproveRequest() {
+		// Add an app to be approved
+		dev.submitRequest(AOT, APP);
+		Admin admin = new Admin("testAdmin", "test1234");
+		admin.approveRequest(AOT, APP);
+		ArrayList<Application> reference = new ArrayList<Application>();
+		assertEquals("Arraylists not equal", AOT.getApps(), reference);
+	}
+}
