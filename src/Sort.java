e0cf543a45bb8c3729c63d43f9f2ebc5b07b64e4
Qi Feng <49265120+Rivendell-FQ@users.noreply.github.com>
2020-11-17 16:40:36 -0500

    comments

--- src/Sort.java
+++ src/Sort.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 
-// platform
+/*
+the platform filter
+*/
 
 public class Sort {
 	private ArrayList<String> sorts;
@@ -12,11 +14,16 @@ public class Sort {
 		data = new ArrayList<ArrayList<String>>(AppOrganizationTool.sortFilterData);
 		
 	}
-	
+	/* make the arraylist of the platform sort
+	* 
+	*/
 	public void setSorts(ArrayList<String> sorts) {
 		this.sorts = new ArrayList<String>(sorts);
 	}
-
+/*
+* return the arraylist that have been already filtered
+* use arraylist to filter the platforms sdorts
+*/
 	public ArrayList<ArrayList<String>> getFilteredSorts() {
 		ArrayList<ArrayList<String>> filtered = new ArrayList<ArrayList<String>>();
 		System.out.println("Platform Sort");
