fb67352a8da164bd76af5497ec951fa77c29d983
Qi Feng <49265120+Rivendell-FQ@users.noreply.github.com>
2020-11-18 19:24:25 -0500

    comments

--- src/PlatformFilter.java
+++ src/PlatformFilter.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PlatformFilter {
-
+        //create constant arraylist
 	private ArrayList<String> platforms;
 	private ArrayList<ArrayList<String>> data;
 
@@ -13,21 +13,32 @@ public class PlatformFilter {
 		data = new ArrayList<ArrayList<String>>(AppOrganizationTool.sortPlatformFilterData);
 		
 	}
-	
-	public void setGenres(ArrayList<String> platforms) {
+	/*
+	* make the arraylist for the platforms
+	*
+	*@param genres the platforms arraylist of the application
+	*@return void
+	*/
+	public void setPlatforms(ArrayList<String> platforms) {
 		this.platforms = new ArrayList<String>(platforms);
 	}
-
+     /*
+      * make the platforms of the filter been filter
+      *
+      * @return the arraylist that are filtered
+      */
 	public ArrayList<ArrayList<String>> getFilteredPlatforms() {
 		ArrayList<ArrayList<String>> filtered = new ArrayList<ArrayList<String>>();
 		System.out.println("FilterPlatform");
 		boolean isWrong = false;
+		// using for each method in the arrraylist to filter the platforms data
 		for (ArrayList<String> a : data) {
 			for (int i = 0; i < platforms.size(); i++) {
 				if (a.get(3).indexOf(platforms.get(i)) == -1) {
 					isWrong = true;
 				}
 			}
+			// if no wrong, the platforms data will be added to the filtered arraylist
 			if (!isWrong) {
 				filtered.add(a);
 			}
