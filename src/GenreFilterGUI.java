851acce3fe149d743af802ebcbbb759058f2cb22
Qi Feng <49265120+Rivendell-FQ@users.noreply.github.com>
2020-11-18 19:03:31 -0500

    comments and update

--- src/GenreFilterGUI.java
+++ src/GenreFilterGUI.java
@@ -2,11 +2,10 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-
 import javax.swing.*;
 
 public class GenreFilterGUI extends JPanel implements ItemListener, ActionListener{
-	
+	// create constant arraylist and constant variables
 	private ArrayList<String> availableGenres;
 	public ArrayList<String> activeGenres;
 	public ArrayList<JCheckBox> checkBoxes;
@@ -17,10 +16,13 @@ public class GenreFilterGUI extends JPanel implements ItemListener, ActionListen
 	private GenreFilter filter;
 	private ArrayList<ArrayList<String>> data;
 	
+	/*
+	* the GUI of the filter to the genre of the applications
+	*@return show the GUI part of it
+	*/
 	public GenreFilterGUI() {
-		
-		super();
-		
+		super();  // using the data in the super class of GenreFilter
+		// set up the JPanel and arraylist
 		filter = new GenreFilter();
 		data = new ArrayList<ArrayList<String>>();
 		this.setLayout(new BorderLayout());
@@ -29,59 +31,67 @@ public class GenreFilterGUI extends JPanel implements ItemListener, ActionListen
 		this.buttons = new JPanel();
 		buttons.setLayout(new FlowLayout());
 		
-		this.availableGenres = new ArrayList<String>(Arrays.asList("Education","Chat","Games","Photo","Music","Shopping","News"));
+		//add these 9 genres to the availableGnres arraylist of the genres
+		this.availableGenres = new ArrayList<String>(Arrays.asList("Education","Chat","Game","Photo","Music","Shopping","News","Weather","Book"));
 		
 		this.activeGenres = new ArrayList<String>();
 		
 		checkBoxes = new ArrayList<JCheckBox>();
-			
+		
+		// add these 9 genres to the JCheckBox
 		JCheckBox Education = new JCheckBox("Education", false);
 		JCheckBox Chat = new JCheckBox("Chat", false);
-		JCheckBox Games = new JCheckBox("Games", false);
+		JCheckBox Games = new JCheckBox("Game", false);
 		JCheckBox Photo = new JCheckBox("Photo", false);
 		JCheckBox Music = new JCheckBox("Music", false);
 		JCheckBox Shopping = new JCheckBox("Shopping", false);
 		JCheckBox News = new JCheckBox("News", false);
+		JCheckBox Weather = new JCheckBox("Weather",false);
+		JcheckBox Book = new JCheckBox("Book",false);
 		
+		// add these nine genres to the checkBoxes
 		checkBoxes.add(Education);
 		checkBoxes.add(Chat);
-		checkBoxes.add(Games);
+		checkBoxes.add(Game);
 		checkBoxes.add(Photo);
 		checkBoxes.add(Music);
 		checkBoxes.add(Shopping);
 		checkBoxes.add(News);
+		checkBoxes.add(Weather);
+		checkBoxes.add(Book);
 	 
-		
+		// using for each method in arraylist to add addItemListener to the checkBoxList
 		for (JCheckBox c: checkBoxes) {
 			c.addItemListener(this);
 			checkBoxList.add(c);
 		}
 		
+		// Add the JLabel of "Filter by Genre" and two JButton of "Filter" and "Cancel" which can be clicked
 		JLabel filterLabel = new JLabel("Filter by Genre");
 		filterButton = new JButton("Filter");
 		cancelButton = new JButton("Cancel");
 		filterButton.addActionListener(this);
 		cancelButton.addActionListener(this);
-		
+		// add the two JButtons
 		buttons.add(filterButton);
 		buttons.add(cancelButton);
-		
+		// Add the filterLabel, 9 genres of the checkBoxList, and the two buttons to the GUI
 		this.add(filterLabel, BorderLayout.NORTH);
 		this.add(checkBoxList, BorderLayout.CENTER);
 		this.add(buttons, BorderLayout.SOUTH);
 		
 	}
 
+	// the override auto-genrated method for the GUI
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
 
+	// the override auto-genrated method for the GUI
 	@Override
 	public void itemStateChanged(ItemEvent arg0) {
 		// TODO Auto-generated method stub
-		
 	}
 
  
